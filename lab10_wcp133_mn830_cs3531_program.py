3f282a7136c1cdb5d26f5966e71e760780c5cb93
ParkerTwin1 <42685457+ParkerTwin1@users.noreply.github.com>
2018-11-12 13:15:58 -0600

    Update lab10_wcp133_mn830_cs3531_program.py

--- lab10_wcp133_mn830_cs3531_program.py
+++ lab10_wcp133_mn830_cs3531_program.py
@@ -1,1 +1,18 @@
 # Lab 10
+
+from PIL import Image
+img = Imgage.open( "handprint.png" )
+
+img = img.convert( "L" )
+
+# image dimesnions 
+cols = img.size[0] #width
+rows = img.size[1] #height
+
+pix = img.load()
+
+print( "pixel at row", 3, "and col", 4, "is", pix[4, 3] )
+
+for row in range(rows):
+  for col in range(cols):
+    pix [col, row] = 255 - pix[ col, row ]
